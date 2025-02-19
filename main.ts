7181a1768e83989f844e102f726ce87510052daf
Mame Mor Mbacke <60527044+Mame-Mor-M@users.noreply.github.com>
2025-02-19 16:48:45 -0500

    Removed all js code from main

--- main.ts
+++ main.ts
@@ -1,17 +1,1 @@
-namespace SpriteKind {
-    export const Sign = SpriteKind.create()
-    export const Item = SpriteKind.create()
-    export const OwnedItem = SpriteKind.create()
-}
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
-	
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`Door tile`, function (sprite, location) {
-	
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-	
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-	
-})
+
