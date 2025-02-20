380c63ad369b50132fdb39b88aeea93a44df42b8
Mame Mor Mbacke <60527044+Mame-Mor-M@users.noreply.github.com>
2025-02-19 19:21:18 -0500

    Updated riddle2 descriptions

--- Riddle2.md
+++ Riddle2.md
@@ -1,20 +1,24 @@
-# Time to Decorate!
-Hello ninjas, it's time to decorate! As codey makes his way back to the center he creates a map pinpointing the locations where each decoration will be placed. Your task is to make sure all decorations are placed in the correct location
-## Step 1
-Cody wants to place ninja stars all along the board but some whiteboard pieces are missing, fix the code so that Cody can get to the whiteboard
-## Step 2
-The Dojo is finally complete and now it's time to decorate! Cody wants to place down all the decorations he bought
+# Time to Decorate! @showdialog
+Hello ninjas, it's time to decorate! 
 
-Fix the code so Cody can pick up his decorations and drop them off around the dojo. Players should be able to use the A button to place a decoration
+As codey makes his way back to the center he creates a map pinpointing the locations where each decoration will be placed. Your task is to make sure all decorations are placed in the correct location
+
+## Step 1
+It's a mess in here! 
+
+Add more obstacle sprites around the dojo that Cody must avoid to reach the ninja lab, use the given assets or create sprites of things you may see around the dojo.
+
+## Step 2
+Cody wants to place ninja stars all along the board but some whiteboard pieces are missing, fix the code so that Cody can get to the whiteboard.
+
+Complete the tilemap so that there are no empty squares.
+
 ## Step 3
+The obstacles don't do anything! Edit the code to make sure overlapping an obstacle decreases player health
 
 ## Step 4
+The Dojo is finally complete and now it's time to decorate! Cody wants to place down all the decorations he bought. Fix the code so that Cody can drop his decorations off at each designated location.
 
-## Step 5
 
-```template
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
-    Shop_Sign.sayText("Welcome to the " + Fake_Name, 1000, false)
-    sprite.setPosition(130, 250)
-})
-```
+## Step 5 @showdialog
+THE ANSWER IS ATTACK
--- main.ts
+++ main.ts
@@ -2,80 +2,19 @@ namespace SpriteKind {
     export const OwnedItem = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
-    Shop_Sign.sayText("Welcome to the " + Fake_Name, 1000, false)
-    sprite.setPosition(130, 250)
+	
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`Door tile`, function (sprite, location) {
-    Shop_Sign.sayText("Welcome to the " + Fake_Name, 1000, false)
-    sprite.setPosition(130, 225)
+	
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-    if (info.score() < 4) {
-        otherSprite.follow(sprite, 40)
-        otherSprite.setKind(SpriteKind.OwnedItem)
-        sprite.sayText("Purchased: Ninja Star", 1000, false)
-    } else {
-        otherSprite.sayText("Not enough coins")
-    }
+	
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     info.changeScoreBy(1)
 })
-let Fake_Name = ""
-let Shop_Sign: Sprite = null
-tiles.setCurrentTilemap(tilemap`Store`)
+tiles.setCurrentTilemap(tilemap`Dojo`)
 let Cody = sprites.create(assets.image`Ninja`, SpriteKind.Player)
 tiles.placeOnTile(Cody, tiles.getTileLocation(8, 4))
-Shop_Sign = sprites.create(assets.image`Sign`, SpriteKind.Enemy)
-tiles.placeOnTile(Shop_Sign, tiles.getTileLocation(9, 11))
-scene.cameraFollowSprite(Cody)
 controller.moveSprite(Cody)
-Fake_Name = "N1nnja Sh00p"
-let Real_Shop_Name = "Ninja Shop"
-let NinjaStar = sprites.create(assets.image`Ninja Star`, SpriteKind.Projectile)
-tiles.placeOnTile(NinjaStar, tiles.getTileLocation(4, 7))
-let NinjaCoins = sprites.create(img`
-    . . . b b b . . 
-    . . b 5 5 5 b . 
-    . b 5 d 3 d 5 b 
-    . b 5 1 5 3 5 b 
-    . c d 1 5 3 5 c 
-    . c d d 1 d 5 c 
-    . . f d d d f . 
-    . . . f f f . . 
-    `, SpriteKind.Food)
-tiles.placeOnTile(NinjaCoins, tiles.getTileLocation(4, 2))
-NinjaCoins = sprites.create(img`
-    . . . b b b . . 
-    . . b 5 5 5 b . 
-    . b 5 d 3 d 5 b 
-    . b 5 1 5 3 5 b 
-    . c d 1 5 3 5 c 
-    . c d d 1 d 5 c 
-    . . f d d d f . 
-    . . . f f f . . 
-    `, SpriteKind.Food)
-tiles.placeOnTile(NinjaCoins, tiles.getTileLocation(8, 2))
-NinjaCoins = sprites.create(img`
-    . . . b b b . . 
-    . . b 5 5 5 b . 
-    . b 5 d 3 d 5 b 
-    . b 5 1 5 3 5 b 
-    . c d 1 5 3 5 c 
-    . c d d 1 d 5 c 
-    . . f d d d f . 
-    . . . f f f . . 
-    `, SpriteKind.Food)
-tiles.placeOnTile(NinjaCoins, tiles.getTileLocation(14, 5))
-NinjaCoins = sprites.create(img`
-    . . . b b b . . 
-    . . b 5 5 5 b . 
-    . b 5 d 3 d 5 b 
-    . b 5 1 5 3 5 b 
-    . c d 1 5 3 5 c 
-    . c d d 1 d 5 c 
-    . . f d d d f . 
-    . . . f f f . . 
-    `, SpriteKind.Food)
-tiles.placeOnTile(NinjaCoins, tiles.getTileLocation(1, 5))
-info.setScore(0)
+scene.cameraFollowSprite(Cody)
